ccad9c102205d1a324204fb205fff80ed0029ddf
Milenko Castillo <milenko.castillo@gmail.com>
2023-08-31 13:37:10 -0400

    playlist response

--- src/spotify.types.ts
+++ src/spotify.types.ts
@@ -75,6 +75,34 @@ export interface ArtistResponse {
   uri:           string;
 }
 
+export interface PlaylistResponse {
+  collaborative: boolean;
+  description:   string;
+  external_urls: ExternalUrls;
+  followers:     Followers;
+  href:          string;
+  id:            string;
+  images:        Image[];
+  name:          string;
+  owner:         Owner;
+  public:        boolean;
+  snapshot_id:   string;
+  tracks:        Tracks;
+  type:          string;
+  uri:           string;
+}
+
+export interface Owner {
+  external_urls: ExternalUrls;
+  followers?:    Followers;
+  href:          string;
+  id:            string;
+  type:          string;
+  uri:           string;
+  display_name?: string;
+  name?:         string;
+}
+
 export interface AlbumAlbums {
   album_type:             'album' | 'compilation' | 'single';
   total_tracks:           number;
